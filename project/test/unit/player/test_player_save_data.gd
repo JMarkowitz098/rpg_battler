ac0981fc4e46c65780532ab8c0736da76f193e44
Jared <jmarkowitz098@gmail.com>
2024-07-08 14:23:53 -0700

    finish tests for player_save_data

--- project/test/unit/player/test_player_save_data.gd
+++ project/test/unit/player/test_player_save_data.gd
@@ -3,14 +3,14 @@ extends GutTest
 var TestPlayerSaveData:= load("res://players/player_save_data.gd")
 var _data: PlayerSaveData = null
 
-func before_each() -> void:
-	var init := {
+var init := {
  		"level": 1,
  		"player_id": Stats.PlayerId.TALON,
  		"slot": 0,
  		"unique_id": "1234"
 	}
 
+func before_each() -> void:
 	_data = TestPlayerSaveData.new(init)
 
 func test_create_object() -> void:
@@ -18,3 +18,6 @@ func test_create_object() -> void:
 	assert_eq(_data.player_id, Stats.PlayerId.TALON, "Created with correct player_id")
 	assert_eq(_data.slot, 0, "Created with correct slot")
 	assert_eq(_data.unique_id, "1234", "Created with correct unique_id")
+
+func test_format_for_save() -> void:
+	assert_eq(_data.format_for_save(), init, "Format for save correctly creates dictionary")
