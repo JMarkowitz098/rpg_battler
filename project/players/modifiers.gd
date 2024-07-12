c91f8475eb3fd24eefa62f8211d63831b818b8cd
Jared <jmarkowitz098@gmail.com>
2024-07-12 13:56:02 -0700

    Add set current ingress to modifiers

--- project/players/modifiers.gd
+++ project/players/modifiers.gd
@@ -1,12 +1,20 @@
 extends Node
 
+signal ingress_updated
+signal no_ingress
+
 var has_small_refrain_open := false
 var is_dodging := false
 var is_eth_dodging := false
 var rand_agi := 1
-var current_ingress := 1
 var plus_max_ingress := 0
 var plus_incursion := 0
 var plus_refrain := 0
 var plus_agility := 0
 var current_refrain_element := Element.Type.NONE
+var current_ingress := 1 
+
+func set_current_ingress(new_value: int, max_ingress: int) -> void:
+  current_ingress = clamp(new_value, 0, max_ingress)
+  ingress_updated.emit()
+  if current_ingress == 0: no_ingress.emit()
--- project/test/unit/player/test_modifiers.gd
+++ project/test/unit/player/test_modifiers.gd
@@ -3,14 +3,18 @@ extends GutTest
 var TestModifiers := load("res://players/modifiers.gd")
 var modifiers: Node
 
+
 func before_each() -> void:
 	modifiers = TestModifiers.new()
+
 
 func after_each() -> void:
 	modifiers.free()
 
+
 func test_can_create_modifiers() -> void:
 	assert_not_null(modifiers)
+
 
 func test_default_values() -> void:
 	var keys_and_expected := [
@@ -30,4 +34,32 @@ func test_default_values() -> void:
 		assert_eq(modifiers[key_val[0]], key_val[1])
 
 
-	
+func test_set_current_ingress() -> void:
+	var tests: Array[Array] = [
+		# ["Test description"_action_has_unique_id, new_value, expected, max_ingress]
+		["It can set current ingress", 5, 5, 10],
+		["It sets it at a min of 0", -3, 0, 10],
+		["It sets it at a max of max_ingress", 15, 10, 10]
+	]
+
+	for test in tests:
+		_test_set_current_ingress(test)
+
+
+func test_signals() -> void:
+	watch_signals(modifiers)
+
+	gut.p("It emits an ingress_updated signal when ingress changes")
+	modifiers.set_current_ingress(5, 10)
+	assert_signal_emitted(modifiers, "ingress_updated")
+
+	gut.p("It emits a no_ingress_signal when ingress is set to zero or less")
+	modifiers.set_current_ingress(0, 10)
+	assert_signal_emitted(modifiers, "no_ingress")
+
+
+func _test_set_current_ingress(values: Array) -> void:
+	var expected: int = values[2]
+	modifiers.set_current_ingress(values[1], values[3])
+	var actual: int = modifiers.current_ingress
+	assert_eq(expected, actual, values[0])
