35cb73a17c1e633641f314539abd5e2b7584c913
Jared <jmarkowitz098@gmail.com>
2024-07-16 08:51:39 -0700

    Update save_file_data class

---
+++ project/save_data/save_file_data.gd
@@ -0,0 +1,18 @@
+extends Resource
+class_name SaveFileData
+
+var id: String
+var players_data: Array[PlayerData]
+var save_time: String
+var round_number: Round.Number
+
+func _init(
+  _id: String,
+  _players_data: Array[PlayerData],
+  _save_time: String,
+  _round_number: Round.Number
+) -> void:
+  id = _id
+  players_data = _players_data
+  save_time = _save_time
+  round_number = _round_number
