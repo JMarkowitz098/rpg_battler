563622b203ee6f6a9d16ead639711ceb89e23f5b
Jared <jmarkowitz098@gmail.com>
2024-07-18 18:38:20 -0700

    Add typing to animation creator

--- project/players/animation_creator.gd
+++ project/players/animation_creator.gd
@@ -2,15 +2,15 @@ extends Node2D
 
 @export var ANIMATIONS_SCENE: PackedScene
 
-var animations
+var animations: Variant
 
-func create_animations():
+func create_animations() -> Variant:
 	if not ANIMATIONS_SCENE is PackedScene: return
-	var world = get_tree().current_scene
+	var world := get_tree().current_scene
 	animations = ANIMATIONS_SCENE.instantiate()
 	world.add_child.call_deferred(animations)
 	animations.global_position = global_position
 	return animations
 
-func get_animations_node():
+func get_animations_node() -> Variant:
 	return animations
